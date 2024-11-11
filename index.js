06788c7d4506c6fc60a714734d63782e71c6d28f
Fabishhhh <fafa.verhulst@gmail.com>
2024-11-11 19:23:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import dotenv from 'dotenv';
 import cowsay from 'cowsay';
 
-// Charger les variables d'environnement depuis .env
 dotenv.config();
 
-// Utiliser les variables d'environnement
 const message = `Hello I'm ${process.env.NAME} from ${process.env.CAMPUS}!`;
 
-// Afficher la vache avec le message
 console.log(cowsay.say({ text: message }));
